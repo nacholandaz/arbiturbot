3fbc6d1ca7ceb09b1809da1380d75817a2678d95
Ricardo Alanis <ricardo.alanis@gmail.com>
2020-09-02 21:01:57 -0500

    Changes to chat api, making it configurable

--- vendors/chat_api.py
+++ vendors/chat_api.py
@@ -8,6 +8,7 @@ import cli
 import log_handler
 
 CHAT_TOKEN = os.getenv('CHAT_TOKEN')
+CHAT_TOKEN = os.getenv('CHAT_URL')
 
 
 def natural_reply_time(reply_text):
@@ -20,7 +21,7 @@ def mark_as_read_and_wait(reply_text, message):
         'read': True,
         'chatId': user_id
     }
-    requests.post(f'https://eu87.chat-api.com/instance99459/sendMessage?token={CHAT_TOKEN}', data=meta_chat).json()
+    requests.post(f'{CHAT_URL}sendMessage?token={CHAT_TOKEN}', data=meta_chat).json()
     # Read, then wait proportionally to writing time
     natural_reply_time(reply_text)
     return True
@@ -35,7 +36,7 @@ def reply(reply_text, message, canonical = True):
     log_handler.insert_message(meta_chat)
     if cli.is_on() == False:
         print(f'Sending text {reply_text} to chat api...')
-        r = requests.post(f'https://eu87.chat-api.com/instance99459/sendMessage?token={CHAT_TOKEN}', data=meta_chat).json()
+        r = requests.post(f'{CHAT_URL}sendMessage?token={CHAT_TOKEN}', data=meta_chat).json()
     else:
         cli.puts_reply(meta_chat)
     if user.get(user_id):
@@ -48,15 +49,15 @@ def reply(reply_text, message, canonical = True):
 def set_webhook(webhook_url):
     meta_chat = {
         "set": True,
-        "webhookUrl": "http://d53eb69b.ngrok.io/messages"
+        "webhookUrl": webhook_url
     }
-    return requests.post(f'https://eu87.chat-api.com/instance99459/webhook?token={CHAT_TOKEN}', data=meta_chat).json()
+    return requests.post(f'{CHAT_URL}webhook?token={CHAT_TOKEN}', data=meta_chat).json()
 
 def get_chats():
-    return requests.get(f'https://eu87.chat-api.com/instance99459/chats?token={CHAT_TOKEN}').json()
+    return requests.get(f'{CHAT_URL}chats?token={CHAT_TOKEN}').json()
 
 def get_messages():
-    return requests.get(f'https://eu87.chat-api.com/instance99459/messages?token={CHAT_TOKEN}').json()
+    return requests.get(f'{CHAT_URL}messages?token={CHAT_TOKEN}').json()
 
 def get_chat_user_name(user_id):
     try:
